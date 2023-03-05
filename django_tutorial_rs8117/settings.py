2d28e0e394bab594495d01ca2efd6244b9eed64d
ramanarayanan-98 <ramanarayanan.s.1998@gmail.com>
2023-03-05 18:14:50 -0500

    Secret key entered

--- django_tutorial_rs8117/settings.py
+++ django_tutorial_rs8117/settings.py
@@ -20,6 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y*109gasq3kplx1enn)ez#89003-p+$&a6#4!3&w+38+l7qzoa'
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
